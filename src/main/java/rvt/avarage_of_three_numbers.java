55eaec14923a477f6ef70697894582a493ec8f76
24DP1APužu <A242211AP@rvt.lv>
2025-09-17 09:20:11 +0300

    Average of three numbers

---
+++ src/main/java/rvt/avarage_of_three_numbers.java
@@ -0,0 +1,20 @@
+package rvt;
+import java.util.Scanner;
+
+public class avarage_of_three_numbers{
+    public static void main(String[] arg){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Value of first number:");
+        int firstnm = Integer.valueOf(scanner.nextLine());
+        System.out.println("Value of second number:");
+        int secnum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Value of third number:");
+        int thirdnum = Integer.valueOf(scanner.nextLine());
+        double avrager = (firstnm + secnum + thirdnum) / 3.0;
+        System.out.println("The avarage is " + avrager);
+
+
+    }
+      
+
+}
