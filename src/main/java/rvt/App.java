6617d265523deeac5b468d1275fa2815564ff898
24DP1APužu <A242211AP@rvt.lv>
2025-09-17 09:40:05 +0300

    changed back

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,13 +3,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sumer = new Scanner(System.in);
-        System.out.println("Given the first number:");
-        int firstsk = Integer.valueOf(sumer.nextLine());
-        System.out.println("Given the second number:");
-        int secondsk = Integer.valueOf(sumer.nextLine());
-        int sum = firstsk + secondsk;
-        System.out.println(firstsk + "+" + secondsk +"="+sum);
 
     }
 }
