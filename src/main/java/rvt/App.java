79490cff96fb074069a01815f905f3860d9020eb
24DP1APužu <A242211AP@rvt.lv>
2025-09-17 09:02:39 +0300

    addition formula uzdevums izpildīts, saskaita divus integerus.

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,15 @@
 package rvt;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // Your code goes here...
+        Scanner sumer = new Scanner(System.in);
+        System.out.println("Given the first number:");
+        int firstsk = Integer.valueOf(sumer.nextLine());
+        System.out.println("Given the second number:");
+        int secondsk = Integer.valueOf(sumer.nextLine());
+        int sum = firstsk + secondsk;
+        System.out.println(firstsk + "+" + secondsk +"="+sum);
+
     }
 }
