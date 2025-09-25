60a2cea133287823d8befc3eb099eae53d31acc5
24DP1APužu <A242211AP@rvt.lv>
2025-09-25 13:22:34 +0300

    Number and sum of numbers

---
+++ src/main/java/rvt/numbersum.java
@@ -0,0 +1,25 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class numbersum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int suma = 0;
+        int skskaits = 0;
+        while (true) {
+            System.out.println("Given a number: ");
+            int numba = Integer.valueOf(scanner.nextLine());
+            if (numba == 0) {
+                break;
+            }
+            skskaits++;
+            suma += numba;
+
+        }
+        System.out.println("Number of numbers: " + skskaits);
+        System.out.println("Sum of the numbers: " + suma);
+        scanner.close();
+    }
+
+}
