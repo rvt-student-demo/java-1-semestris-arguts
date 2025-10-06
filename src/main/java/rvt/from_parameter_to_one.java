deeafda340423dce7608dfeda42d8bdc8378dcc7
24DP1APužu <A242211AP@rvt.lv>
2025-10-06 11:07:11 +0300

    From parameter to one

---
+++ src/main/java/rvt/from_parameter_to_one.java
@@ -0,0 +1,13 @@
+package rvt;
+
+public class from_parameter_to_one {
+    public static void main(String[] args) {
+        fromToOne(2);
+    }
+    public static void fromToOne(int numba) {
+        for(int i = numba; i >= 1; i --){
+            System.out.println(i);
+        }
+        
+    }
+}
