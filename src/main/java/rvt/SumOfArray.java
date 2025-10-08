8dd538821ce77e4bc7bc1f604254fcde65d1b2c9
24DP1APužu <A242211AP@rvt.lv>
2025-10-08 09:44:49 +0300

    sumOfArray fixed

--- src/main/java/rvt/SumOfArray.java
+++ src/main/java/rvt/SumOfArray.java
@@ -3,11 +3,16 @@ package rvt;
 public class SumOfArray {
     public static void main(String[] args) {
         int[] numbers = {5, 1, 3, 4, 2};
+        
+        sumOfNumbersInArray(numbers);
+    }
+    public static int sumOfNumbersInArray(int[] array){
         int suma = 0;
-        for(int i = 0; i < numbers.length; i ++){
-           suma += numbers[i];
+        for(int i = 0; i < array.length; i ++){
+           suma += array[i];
         }
         System.out.println(suma);
+        return suma;
     }
     
 }
