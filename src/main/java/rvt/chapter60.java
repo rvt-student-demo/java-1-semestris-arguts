b83cf9a874636bc7da8e49180c1b81c79fa4a1f1
24DP1APužu <A242211AP@rvt.lv>
2025-10-09 12:40:14 +0300

    chapter60 ex2

--- src/main/java/rvt/chapter60.java
+++ src/main/java/rvt/chapter60.java
@@ -2,14 +2,34 @@ package rvt;
 
 public class chapter60 {
     public static void main(String[] args) {
-        ex1(args);
+        ex2(args);
     }
-    public static void ex1(String[] args){
-        int[] numbers = {0, 5, 5, 5};
+
+    public static void ex1(String[] args) {
+        int[] numbers = { 0, 5, 5, 5 };
         int sum = 0;
-        for(int i = 0; i < numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
         System.out.println(sum);
+    }
+
+    public static void ex2(String[] args) {
+        int[] val = { 13, -4, 82, 17 };
+        int[] twice = val;
+
+        System.out.println("Original Array: ");
+        for(int i = 0; i < val.length; i++){
+            System.out.println(val[i]);
+        }
+        // Construct an array object for twice.
+
+        // Put values in twice that are twice the
+        // corresponding values in val.
+        System.out.println("Twice Array: ");
+        for(int i = 0; i < twice.length; i++){
+            System.out.println(twice[i] * 2);
+        }
+        
     }
 }
