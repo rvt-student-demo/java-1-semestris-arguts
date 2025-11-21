748cc338cadb427805fe239fcefbbb7088ae82bd
24DP1APužu <A242211AP@rvt.lv>
2025-11-21 11:46:04 +0200

    fixed

---
+++ src/main/java/rvt/pieskaite.java
@@ -0,0 +1,11 @@
+package rvt;
+
+public class pieskaite {
+    public static void main(String[] args) {
+        int sk = 0;
+        while (true) {
+            sk++;
+            System.out.println(sk);
+        }
+    }
+}
