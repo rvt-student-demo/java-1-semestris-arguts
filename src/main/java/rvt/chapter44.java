5515f160fcc2d68e8479a4c6ab362bf725afd08e
24DP1APužu <a242211ap@rvt.lv>
2025-12-01 22:48:02 +0200

    started chapter 44

---
+++ src/main/java/rvt/chapter44.java
@@ -0,0 +1,23 @@
+package rvt;
+
+
+import java.util.Scanner; 
+
+public class chapter44 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public static void ex1(Scanner scanner) {
+        
+    }
+    public static void ex2() {}
+    public static void ex3() {}
+    public static void ex4() {}
+    public static void ex5() {}
+    public static void ex6() {}
+    public static void ex7() {}
+
+}
+
